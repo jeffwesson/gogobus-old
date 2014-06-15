e9fa7824f86f1631126156d65e0c71cd6b019500
jeffwesson <jeff@jeffwesson.com>
2014-06-14 17:53:12 -0700

    angular-fullstack w/ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,34 @@
 'use strict';
 
-angular.module('gogobusApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize'
-]);
+angular
+  .module('gogobusApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('main',{
+        url: '/main',
+        templateUrl: 'views/partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('about',{
+        url: '/about',
+        templateUrl: 'views/partials/about.html',
+        controller: 'MainCtrl'
+      })
+      .state('contact',{
+        url: '/contact',
+        templateUrl: 'views/partials/contact.html',
+        controller: 'MainCtrl'
+      })
+      .state('main.list',{
+        url: '/list',
+        templateUrl: 'views/partials/list.html',
+        controller: 'MainCtrl'
+      });
+    $urlRouterProvider
+      .otherwise('/main');
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('gogobusApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $location) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
   });
